6209dc5d8d270e0a1cac1e35cbe9728056cbee7c
Aniketbobade <bobadeaniket5@gmail.com>
2023-03-22 16:31:08 +0530

    Update Cards.js

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -6,7 +6,7 @@ function Cards(props){
 
     return (
         <div className='mx-auto text-white'>
-        <img className='mx-auto rounded-full' src={review.image} alt="photo" />
+        <img className='mx-auto rounded-full' src={review.image} alt="" />
         <p className='text-[20px] font-bold'><span>Name :-</span>{review.name}</p>
         <p className='text-[20px]'><span>Role :-</span>{review.job}</p>
         <p className='text-[20px]'>{review.text}</p>
